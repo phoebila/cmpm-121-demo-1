22baca90880cca4b2f8e95366fe1c245a1439c12
Phoebe <128556139+phoebila@users.noreply.github.com>
2024-10-04 17:05:36 -0700

    Step one complete

--- src/main.ts
+++ src/main.ts
@@ -2,10 +2,24 @@ import "./style.css";
 
 const app: HTMLDivElement = document.querySelector("#app")!;
 
-const gameName = "My game";
+const gameName = "Application Clicker";
 
 document.title = gameName;
 
 const header = document.createElement("h1");
 header.innerHTML = gameName;
+
+// Step 1 - Button
+const button = document.createElement("button");
+let appCount = 0;
+button.textContent = '📄';
+button.addEventListener('click', () => {
+    appCount++;
+});
+button.style.padding = '10px 10px';
+button.style.fontSize = '28px';
+button.style.marginTop = '10px';
+console.log(appCount);
+
 app.append(header);
+app.append(button);
