b322dbee45d2881a4ea7d1965f2c28b530f3c958
Phoebe <128556139+phoebila@users.noreply.github.com>
2024-10-04 17:15:36 -0700

    Step 3 Completed!

--- src/main.ts
+++ src/main.ts
@@ -18,10 +18,21 @@ const appDisplay = document.createElement('div');
 let appCount: number = 0;
 appDisplay.textContent = `${appCount} applications have been sent!`;
 
+// listener for when the button is clicked
 button.addEventListener('click', () => {
     appCount++;
     appDisplay.textContent = `${appCount} applications have been sent!`;
 });
+
+// Step 3 - Automatic clicker!
+const applicationClicker = () => {
+    appCount++;
+    appDisplay.textContent = `${appCount} applications have been sent!`;
+}
+
+setInterval(applicationClicker, 100);
+
+// styling the button
 button.style.padding = '10px 10px';
 button.style.fontSize = '28px';
 button.style.marginTop = '10px';
